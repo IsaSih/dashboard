0f92f9baaecb3d3022c9f1f92d1e71499cd988ba
Isabela Guimaraes <isabela.guimaraes@suse.com>
2026-03-26 14:28:47 -0700

    testisolation-on
    Signed-off-by: Isabela Guimaraes <isabela.guimaraes@suse.com>

--- cypress/e2e/tests/pages/generic/about.spec.ts
+++ cypress/e2e/tests/pages/generic/about.spec.ts
@@ -4,9 +4,15 @@ import DiagnosticsPagePo from '@/cypress/e2e/po/pages/diagnostics.po';
 
 const aboutPage = new AboutPagePo();
 
-describe('About Page', { testIsolation: 'off', tags: ['@generic', '@adminUser', '@standardUser'] }, () => {
-  before(() => {
+describe('About Page', { testIsolation: 'on', tags: ['@generic', '@adminUser', '@standardUser'] }, () => {
+  let navAboutPage = true;
+
+  beforeEach(() => {
     cy.login();
+    if (!navAboutPage) {
+      aboutPage.goTo();
+      aboutPage.waitForPage();
+    }
   });
 
   it('can navigate to About page', () => {
@@ -15,17 +21,15 @@ describe('About Page', { testIsolation: 'off', tags: ['@generic', '@adminUser', 
     aboutPage.waitForPage();
   });
 
-  it('no Prime info when community', { tags: '@noPrime' }, () => {
-    HomePagePo.goToAndWaitForGet();
-    AboutPagePo.navTo();
-    aboutPage.waitForPage();
+  navAboutPage = false;
 
+  it('no Prime info when community', { tags: '@noPrime' }, () => {
     aboutPage.rancherPrimeInfo().should('not.exist');
   });
 
   it('can navigate to Diagnostics page', () => {
-    AboutPagePo.navTo();
-    aboutPage.waitForPage();
+    // AboutPagePo.navTo();
+    // aboutPage.waitForPage();
     aboutPage.diagnosticsBtn().click();
 
     const diagnosticsPo = new DiagnosticsPagePo();
@@ -34,22 +38,27 @@ describe('About Page', { testIsolation: 'off', tags: ['@generic', '@adminUser', 
   });
 
   it('can View release notes', () => {
-    AboutPagePo.navTo();
-    aboutPage.waitForPage();
+    // AboutPagePo.navTo();
+    // aboutPage.waitForPage();
 
     cy.getRancherVersion().then((version) => {
       const isPrime = version.RancherPrime === 'true';
-      const expectedUrl = isPrime ? 'https://documentation.suse.com/cloudnative/rancher-manager/latest/en/release-notes.html' : 'https://github.com/rancher/rancher/releases/tag/';
+      const expectedOrigin = isPrime ? 'https://documentation.suse.com' : 'https://github.com';
+      const expectedPath = isPrime ? '/cloudnative/rancher-manager/latest/en/release-notes' : '/rancher/rancher/releases/tag/';
 
       aboutPage.clickVersionLink('View release notes');
-      cy.location('pathname').should('include', expectedUrl);
+      cy.origin(expectedOrigin, { args: { expectedPath } }, ({ expectedPath }) => {
+        cy.location('pathname').should('include', expectedPath);
+        cy.get('body').should('be.visible');
+        cy.go('back');
+      });
     });
   });
 
   describe('Versions', () => {
-    beforeEach(() => {
-      aboutPage.goTo();
-    });
+    // beforeEach(() => {
+    //   aboutPage.goTo();
+    // });
 
     it('can see rancher version', () => {
       // Check Rancher version
@@ -62,29 +71,37 @@ describe('About Page', { testIsolation: 'off', tags: ['@generic', '@adminUser', 
 
     it('can navigate to /rancher/rancher', () => {
       aboutPage.clickVersionLink('Rancher');
-      cy.origin('https://github.com/rancher/rancher', () => {
-        cy.url().should('include', 'https://github.com/rancher/rancher');
+      cy.origin('https://github.com', () => {
+        cy.location('pathname').should('eq', '/rancher/rancher');
+        cy.get('body').should('be.visible');
+        cy.go('back');
       });
     });
 
     it('can navigate to /rancher/dashboard', () => {
       aboutPage.clickVersionLink('Dashboard');
-      cy.origin('https://github.com/rancher/dashboard', () => {
-        cy.url().should('include', 'https://github.com/rancher/dashboard');
+      cy.origin('https://github.com', () => {
+        cy.location('pathname').should('eq', '/rancher/dashboard');
+        cy.get('body').should('be.visible');
+        cy.go('back');
       });
     });
 
     it('can navigate to /rancher/helm', () => {
       aboutPage.clickVersionLink('Helm');
-      cy.origin('https://github.com/rancher/helm', () => {
-        cy.url().should('include', 'https://github.com/rancher/helm');
+      cy.origin('https://github.com', () => {
+        cy.location('pathname').should('eq', '/rancher/helm');
+        cy.get('body').should('be.visible');
+        cy.go('back');
       });
     });
 
     it('can navigate to /rancher/machine', () => {
       aboutPage.clickVersionLink('Machine');
-      cy.origin('https://github.com/rancher/machine', () => {
-        cy.url().should('include', 'https://github.com/rancher/machine');
+      cy.origin('https://github.com', () => {
+        cy.location('pathname').should('eq', '/rancher/machine');
+        cy.get('body').should('be.visible');
+        cy.go('back');
       });
     });
   });
@@ -95,7 +112,7 @@ describe('About Page', { testIsolation: 'off', tags: ['@generic', '@adminUser', 
 
     // workaround to make the following CLI tests work https://github.com/cypress-io/cypress/issues/8089#issuecomment-1585159023
     beforeEach(() => {
-      aboutPage.goTo();
+      // aboutPage.goTo();
       cy.intercept('GET', 'https://releases.rancher.com/cli2/**').as('download');
     });
 
@@ -155,7 +172,7 @@ describe('About Page', { testIsolation: 'off', tags: ['@generic', '@adminUser', 
     }
 
     beforeEach(() => {
-      cy.login();
+      // cy.login();
       interceptVersionAndSetToPrime().as('rancherVersion');
     });
 
